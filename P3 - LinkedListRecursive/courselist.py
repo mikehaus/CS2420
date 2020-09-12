199379a7fdf93a95c02c08039fc1d5e436cad721
mikehaus <mike.haus.design@gmail.com>
2020-09-11 19:38:54 -0600

    added recursive removal function. Made whitespace and other basic edits to get pylint score up

--- P3 - LinkedListRecursive/courselist.py
+++ P3 - LinkedListRecursive/courselist.py
@@ -1,10 +1,10 @@
-from course import Course
-
 """
+CourseList Class declaration
 CS2420 - P3 Recursive Linked List
 courselist.py
 Mike Hollingshaus
 """
+from course import Course
 
 #-------- START Courselist Class Definition -------#
 
@@ -21,23 +21,25 @@ class CourseList():
         self.head = None
         self.listsize = 0
 
-    def insert(self, Course):
+    def insert(self, course):
         """
         Inserts course Course into list in ascending course number order
         """
-        if self.size() == 0: 
-            self.head = Course
+        if self.size() == 0:
+            self.head = course
             self.size_increment()
             return
-        return self.insert_recursive_helper(self.head, Course)
+        return self.insert_recursive_helper(self.head, course)
 
     def remove(self, number):
         """
         Removes first occurance of course with
         course number number
         """
-        return
-    
+        if self.head is None:
+            return
+        return self.remove_recursive_helper(self.head, number)
+
     def remove_all(self, number):
         """
         Sets head to equal NULL which will get rid of all data in list.
@@ -45,16 +47,16 @@ class CourseList():
         self.head = None
         self.listsize = 0
         return
-    
+
     def find(self, number):
         """
         If target coursenumber not found at head, recursively
         iterates through list. If found returns course number else returns
         -1
         """
-        if self.head == None: 
+        if self.head is None:
             return -1
-        if self.head.number() == number: 
+        if self.head.number() == number:
             return number
         return self.traverse_recursive_find_helper(self.head, number)
 
@@ -81,7 +83,7 @@ class CourseList():
         Calculates GPA based on grade attribute stored in Course Node class.
         Might need to be weighted not sure.
         """
-        if self.size() == 0: 
+        if self.size() == 0:
             return 0.0
         grade_point_total = self.traverse_recursive_gpa_helper(self.head, 0.0)
         return grade_point_total / self.size()
@@ -91,10 +93,10 @@ class CourseList():
         Boolean return value recursively searches through and if sorted returns true.
         Else returns False.
         """
-        if self.head == None: 
+        if self.head is None:
             return True
         return self.traverse_recursive_sorted_helper(self.head.next, self.head.number())
-    
+
     def __str__(self):
         course_string = ''
         return self.traverse_recursive_string_helper(self.head, course_string)
@@ -121,21 +123,20 @@ class CourseList():
         Traverses recursively through list and concatentates
         all Course str functions into a single string.
         """
-        if course == None: 
+        if course is None:
             return course_string
         course_string += str(course)
         return self.traverse_recursive_string_helper(course.next, course_string)
 
-
     def traverse_recursive_find_helper(self, course, target):
         """
         Traverses list recursively and returns Course Node that
-        matches with number value. Returns course number if found 
+        matches with number value. Returns course number if found
         and if not found, returns Null
         """
-        if course == None: 
+        if course is None:
             return -1
-        if course.number() == target: 
+        if course.number() == target:
             return target
         return self.traverse_recursive(course.next, target)
 
@@ -145,7 +146,7 @@ class CourseList():
         matches with number value. Returns Node and if not found, returns
         Null
         """
-        if course == None: 
+        if course is None:
             return grade_point_total
         grade_point_total += course.grade()
         return self.traverse_recursive_gpa_helper(course.next, grade_point_total)
@@ -155,9 +156,9 @@ class CourseList():
         Traverses list recursively and validates classes are in order.
         Returns True if Sorted False if Unsorted
         """
-        if course == None: 
+        if course is None:
             return True
-        if course.number() < last_course_num: 
+        if course.number() < last_course_num:
             return False
         return self.traverse_recursive_sorted_helper(course.next, course.number())
 
@@ -168,17 +169,32 @@ class CourseList():
         if course.number() < node.number():
             course.next = node
             course.prev = node.prev
-            if course.prev != None: course.prev.next = course
+            if course.prev != None:
+                course.prev.next = course
             node.prev = course
-            if node == self.head: self.head = course
+            if node == self.head:
+                self.head = course
             self.size_increment()
             return
-        if node.next == None:
+        if node.next is None:
             node.next = course
             course.prev = node
             self.size_increment()
             return
         self.insert_recursive_helper(node.next, course)
+
+    def remove_recursive_helper(self, node, course_num):
+        """
+        Recursive function that removes node Course from list if
+        course_num matches node.number (coursenumber)
+        """
+        if node is None:
+            return
+        if node.number() == course_num:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            return
+        return self.remove_recursive_helper(node.next, course_num)
 
 #------ END Recursive Helper Methods ------#
 #------- END Courselist Class Definition -----#
@@ -192,7 +208,7 @@ def main():
     course = Course(240, "Discrete Math", 4.0, 4.0)
     courselist.insert(course)
     #print(str(courselist.calculate_gpa()))
-    course = Course(245, "Hardwre", 3.2, 3.2)
+    course = Course(245, "Hardware", 3.2, 3.2)
     courselist.insert(course)
     #print(str(courselist.calculate_gpa()))
     course = Course(300, "Linux", 1.5, 1.5)
@@ -213,6 +229,12 @@ def main():
 
     #print(str(courselist.is_sorted()))
 
+    print(courselist.__str__())
+    print(" ")
+    courselist.remove(240)
+    print(courselist.__str__())
+    course = Course(226, "Hardware AGAIN", 3.5, 3.5)
+    courselist.insert(course)
     print(courselist.__str__())
 
     """print(str(courselist.find(230)))
